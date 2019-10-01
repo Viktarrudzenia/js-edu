399767fa7f7f1d03ad2f09719955714ba9440803
Viktar Rudzenia <viktarrudzenia@gmail.com>
2019-10-01 07:53:28 +0300

    feat: solve task

--- src/index.js
+++ src/index.js
@@ -4,11 +4,12 @@
  * @param config - private student ability to perform for different focus modes
  * @returns number of weeks needed for finish education
  */
+
 module.exports = function getTimeForEducation(
-    focus = 'family', 
-    knowsProgramming = true,
-    config = {family: 4}
-    ) {
-      return 0;
-  };
-  
+  focus = "family",
+  knowsProgramming = true,
+  config = { family: 4 }
+) {
+  let hours = knowsProgramming === true ? 800 : 1300;
+  return Math.ceil(hours / config[focus]);
+};
